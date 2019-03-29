faa7770e4db141507bfd5d4a36365a30c08adf2a
NahushSarje <sarje_nahush@yahoo.co.in>
2019-03-30 00:45:30 +0530

    Add files via upload

---
+++ builder/builder-example-1/UserDTO.java
@@ -0,0 +1,10 @@
+package com.designPattern.builder.builder1;
+
+//Interface implemented by "products"
+//DTO - Data Transfer Object
+public interface UserDTO {
+	
+	String getName();
+	String getAddress();
+	String getAge();
+}
